bffd324badc434236b22af49da8699b2bbe93f90
Alexander Clouter <aclouter@telemetry.com>
2015-03-01 15:35:13 +0000

    'unregistered' checklist with join/leave support

--- public/index.js
+++ public/index.js
@@ -1,25 +1,39 @@
-var map = L.map("map").fitWorld().zoomIn();
+var map = L.map('map').fitWorld().zoomIn();
 
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-	attribution: 'Map data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: "Map data &copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(map);
 
-var sidebar = L.control.sidebar("sidebar").addTo(map);
+var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 var marker = { };
 
-var connection = new WebSocket("ws://" + location.host);
-connection.onmessage = function(e) {
-	var message = JSON.parse(e.data);
-
-	switch (message.type) {
-	case "realtime":
-		if (marker[message.id]) {
-			marker[message.id].addData(message.geojson);
-		} else {
-			marker[message.id] = L.geoJson(message.geojson).addTo(map);
+var tag = 0;
+var connection = new WebSocket('ws://' + location.host);
+connection.onconnection = function() {
+	connection.onclose = function(e) { console.log(e); };
+	connection.onmessage = function(e) {
+		var message = JSON.parse(e.data);
+		switch (message.type) {
+		case 'error':
+			console.log("GISrec ws error: "+message.text);
+			break;
+		case 'realtime':
+			var id = message.geojson.properties.id;
+			if (marker.id === undefined) {
+				marker.id = L.geoJson(message.geojson).addTo(map);
+			} else {
+				marker.id.addData(message.geojson);
+			}
+			break;
 		}
-		break;
-	}
+	};
 };
-connection.onclose = function(e) { console.log(e); };
+
+document.getElementById('unreg').onclick = function() {
+	connection.send(JSON.stringify({
+		tag:		tag++,
+		type:		(this.checked) ? 'join' : 'leave',
+		channel:	null
+	}));
+};
--- server.js
+++ server.js
@@ -1,23 +1,68 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const ws = require("ws");
-const net = require("net");
-const WebSocketServer = require("ws").Server;
-const wss = new WebSocketServer({ server: server }); 
-const es = require("event-stream");
-const fs = require("fs");
+const WebSocketServer = require('ws').Server
+	, http = require('http')
+	, express = require('express')
+	, app = express()
+	, server = http.createServer(app)
+	, wss = new WebSocketServer({ server: server })
+	, net = require('net')
+	, es = require('event-stream')
+	, fs = require('fs');
 
 const KNOTS_TO_METRES_PER_SECOND = 0.51444444444;
 
-app.use("/", express.static(__dirname + "/public"));
+var client = {};
+var channel = {
+	'':	[ ],
+};
 
-wss.on("connection", function (ws) {
-	ws.on("message", function(message) {
-		console.log(message);
+app.use(express.static(__dirname + '/public'));
+
+var iid = 0;
+wss.on('connection', function(ws) {
+	var id = iid++;
+	client.id = ws;
+
+	ws.on('message', function(msg) {
+		var message = JSON.parse(msg);
+
+		if (message.type === 'error') {
+			console.log("error from "+id+": "+message.text);
+			return;
+		}
+
+		if (message.tag === undefined) {
+			ws.send(JSON.stringify({
+				tag:	null,
+				type:	'error',
+				text:	'missing tag',
+			}));
+		}
+
+		switch (message.type) {
+		case 'join':
+			var c = (message.channel !== null) ? c : '';
+			channel[c].push(id);
+			break;
+		case 'leave':
+			var c = (message.channel !== null) ? c : '';
+			channel[c].splice(channel[c].indexOf(id), 1);
+			break;
+		}
+	});
+
+	ws.on('close', function() {
+		Object.keys(channel).forEach(function(c) {
+			delete channel[c].id;
+
+			if (c !== '' && channel.c.length === 0)
+				delete channel.c;
+		});
+
+		delete client.id;
 	});
 });
+
+server.listen(process.env.PORT_HTTP || 27270, '::');
 
 // http://aprs.gids.nl/nmea/#rmc
 const reGPRMC = /^\$GPRMC,([0-9]{6}(?:\.[0-9]+)?)?,([AV])?,([0-9]+(?:\.[0-9]+)?)?,([NS])?,([0-9]+(?:\.[0-9]+)?)?,([EW])?,([0-9]+(?:\.[0-9]+)?)?,([0-9]+(?:\.[0-9]+)?)?,([0-9]{6})?,([0-9]+(?:\.[0-9]+)?)?,([EW])?\*([0-9]+)$/;
@@ -26,51 +71,51 @@ const reGPRMC = /^\$GPRMC,([0-9]{6}(?:\.[0-9]+)?)?,([AV])?,([0-9]+(?:\.[0-9]+)?)
 const reXEXUN = /^([0-9]+),(\+?[0-9]+),(GPRMC,.*,,),[A-Z](\*[0-9]+),([FL]),([^,]*), ?imei:([0-9]*),([0-9]*),([0-9]+(?:\.[0-9]+)?),([FL]):([0-9]+(?:\.[0-9]+)?)V,([01]),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9A-F]+),([0-9A-F]+)$/;
 
 try {
-	fs.statSync("data/.unknown")
+	fs.statSync('data/.unregistered')
 } catch(e) {
 	try {
-		fs.mkdirSync("data");
+		fs.mkdirSync('data');
 	} catch (e) { }
-	fs.mkdirSync("data/.unknown");
+	fs.mkdirSync('data/.unregistered');
 }
 
 var gis = net.createServer(function(client) {
 	var	remoteAddress = client.remoteAddress,
 		remotePort = client.remotePort;
-	console.log("["+remoteAddress+"]:"+remotePort+": connected");
+	console.log('['+remoteAddress+']:'+remotePort+': connected');
 
-	client.on("close", function(client) {
-		console.log("["+remoteAddress+"]:"+remotePort+": disconnected");
+	client.on('close', function(client) {
+		console.log('['+remoteAddress+']:'+remotePort+': disconnected');
 	});
 	client.pipe(es.split()).pipe(es.map(function (data) {
 		var meta = {
-			"id":		null,
-			"raw":		data,
-			"source":	{ address: remoteAddress, port: remotePort },
-			"recv-time":	(new Date())/1000.0,
-			"protocol":	[],
+			'id':		null,
+			'raw':		data,
+			'source':	{ address: remoteAddress, port: remotePort },
+			'recv-time':	(new Date())/1000.0,
+			'protocol':	[],
 		};
 
 		switch (true) {
-		case data === "":
+		case data === '':
 			break;
 		case reXEXUN.test(data):
-			console.log("["+remoteAddress+"]:"+remotePort+": converting xexun");
+			console.log('['+remoteAddress+']:'+remotePort+': converting xexun');
 
 			meta.xexun		= {};
-			meta.protocol.push("xexun");
+			meta.protocol.push('xexun');
 
 			var match = reXEXUN.exec(data);
 			meta.xexun.serial	= match[1];	// gps date + gps time
-			meta.xexun["admin-tel"]	= match[2];
-			data			= "$"+match[3]+match[4];
-			meta.xexun["gps-fix"]	= ( match[5] === "F" ) ? 1 : 0;
+			meta.xexun['admin-tel']	= match[2];
+			data			= '$'+match[3]+match[4];
+			meta.xexun['gps-fix']	= ( match[5] === 'F' ) ? 1 : 0;
 			meta.xexun.message	= match[6];
 			meta.xexun.imei		= match[7];
 			meta.xexun.satellites	= parseInt(match[8]);
 			meta.xexun.altitude	= parseFloat(match[9]);
 			meta.xexun.battery	= {
-				charged: ( match[10] === "F" ) ? 1 : 0,
+				charged: ( match[10] === 'F' ) ? 1 : 0,
 				voltage: parseFloat(match[11]),
 				charging: parseInt(match[12]),
 			};
@@ -85,19 +130,18 @@ var gis = net.createServer(function(client) {
 
 			meta.id = meta.xexun.imei;
 		case reGPRMC.test(data):
-			console.log("["+remoteAddress+"]:"+remotePort+": recording gprmc");
-			meta.protocol.push("gprmc");
-			if (processGPRMC(data, meta) !== undefined);
-				break;
+			console.log('['+remoteAddress+']:'+remotePort+': recording gprmc');
+			meta.protocol.push('gprmc');
+			processGPRMC(data, meta);
+			break;
 		default:
-			console.log("["+remoteAddress+"]:"+remotePort+": unknown format");
+			console.log('['+remoteAddress+']:'+remotePort+': unknown format: '. data);
 			client.end();
 		}
 	}));
 });
 
-server.listen(process.env.PORT_HTTP || 27270, "::");
-gis.listen(process.env.PORT_GIS || 27271, "::");
+gis.listen(process.env.PORT_GIS || 27271, '::');
 
 function processGPRMC(data, properties) {
 	if (properties.id === null) {
@@ -107,7 +151,7 @@ function processGPRMC(data, properties) {
 
 	var match = reGPRMC.exec(data);
 	var	time = match[1],
-		validity = ( match[2] === "A" ) ? 1 : 0,
+		validity = ( match[2] === 'A' ) ? 1 : 0,
 		latitude = parseFloat(match[3]),	hemisphere = match[4],
 		longitude = parseFloat(match[5]),	handedness = match[6],
 		speed = parseFloat((match[7]*KNOTS_TO_METRES_PER_SECOND).toFixed(3)),
@@ -117,7 +161,7 @@ function processGPRMC(data, properties) {
 		checksum = parseInt(match[12]);
 
 	var	dateParts = /([0-9]{2})([0-9]{2})([0-9]{2})/.exec(date);
-	var	d = dateParts[1], m = dateParts[2] - 1, y = "20"+dateParts[3];
+	var	d = dateParts[1], m = dateParts[2] - 1, y = '20'+dateParts[3];
 	var	timeParts = /([0-9]{2})([0-9]{2})([0-9]{2})(?:\.([0-9]{3}))/.exec(time);
 	var	H = timeParts[1], M = timeParts[2], S = timeParts[3], ms = timeParts[4] || 0;
 
@@ -132,22 +176,19 @@ function processGPRMC(data, properties) {
 	properties.gprmc			= {};
 	properties.gprmc.raw			= data;
 	properties.gprmc.speed			= speed;
-	properties.gprmc["course-made-good"]	= cmg;
-	properties.gprmc["magnetic-variance"]	= GPRMC2Degrees(magvar, maghandedness);
+	properties.gprmc['course-made-good']	= cmg;
+	properties.gprmc['magnetic-variance']	= GPRMC2Degrees(magvar, maghandedness);
 	properties.gprmc.checksum		= checksum
 
 	var g = toGeoJSON(multipoint, properties);
 
 	try {
-		fs.statSync("data/"+properties.id)
+		fs.writeFileSync('data/'+properties.id+'/'+ts.toISOString()+'.json', JSON.stringify(g));
+		informRealtime(g, properties.id);
 	} catch(e) {
-		console.log("unregistered device: "+properties.id);
-		informRealtime("unregistered", g);
-		return;
+		fs.writeFileSync('data/.unregistered/'+properties.id+'.json', JSON.stringify(g));
+		informRealtime(g);
 	}
-
-	fs.writeFileSync("data/"+properties.id+"/"+ts.toISOString()+".json", JSON.stringify(g));
-	informRealtime(properties.id, g);
 
 	return g;
 }
@@ -156,7 +197,7 @@ function GPRMC2Degrees(value, direction) {
 	// http://www.mapwindow.org/phorum/read.php?3,16271
 	var d = ((value/100) | 0) + (value - (((value/100) | 0) * 100)) / 60;
 
-	if (direction === "S" || direction === "W")
+	if (direction === 'S' || direction === 'W')
 		d *= -1;
 
 	// http://en.wikipedia.org/wiki/Decimal_degrees#Precision
@@ -167,9 +208,9 @@ function toGeoJSON(multipoint, prop) {
 	prop.time = [];
 
 	var geojson = {
-		type:			"Feature",
+		type:			'Feature',
 		geometry: {
-			type:		"MultiPoint",
+			type:		'MultiPoint',
 			coordinates:	[],
 		},
 		properties:		prop,
@@ -183,8 +224,10 @@ function toGeoJSON(multipoint, prop) {
 	return geojson;
 }
 
-function informRealtime(id, g) {
-	wss.clients.forEach(function(c) {
-		c.send(JSON.stringify({ type: "realtime", id: id, geojson: g }));
+function informRealtime(g, id) {
+	var c = (id !== undefined) ? channel.id : channel[''];
+
+	c.forEach(function(id) {
+		client[id].send(JSON.stringify({ type: 'realtime', geojson: g, id: id }));
 	});
 }
