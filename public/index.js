7f59152d5906dd4b878f0bad59b81fedd6da1d82
Alexander Clouter <aclouter@telemetry.com>
2015-03-12 09:37:19 +0000

    apparently semi-colons are not fasionable

--- public/index.js
+++ public/index.js
@@ -1,16 +1,16 @@
-var debug = $('#debug').hasClass('active');
+var debug = $('#debug').hasClass('active')
 $('#debug').click(function(event) {
-	$(this).button('toggle');
-	debug = $(this).hasClass('active');
-});
+	$(this).button('toggle')
+	debug = $(this).hasClass('active')
+})
 
 var map = L.map('map', {
 	zoomControl: false,
-}).fitWorld().zoomIn();
+}).fitWorld().zoomIn()
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: "Map data &copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
-}).addTo(map);
-L.control.scale().addTo(map);
+}).addTo(map)
+L.control.scale().addTo(map)
 
 var gisControl = L.Control.extend({
 	options: {
@@ -18,39 +18,39 @@ var gisControl = L.Control.extend({
 	},
 
 	onAdd: function (map) {
-		return $('<div class="leaflet-bar leaflet-control"><a title="settings" href="#" data-toggle="modal" data-target="#settings"><i class="fa fa-lg fa-cog"></i></a><a href="#" title="channels" data-toggle="modal" data-target="#channels"><i class="fa fa-lg fa-location-arrow"></i></a></div>').get(0);
+		return $('<div class="leaflet-bar leaflet-control"><a title="settings" href="#" data-toggle="modal" data-target="#settings"><i class="fa fa-lg fa-cog"></i></a><a href="#" title="channels" data-toggle="modal" data-target="#channels"><i class="fa fa-lg fa-location-arrow"></i></a></div>').get(0)
 	},
-});
-map.addControl(new gisControl());
+})
+map.addControl(new gisControl())
 
-var data = new vis.DataSet();
-var layers = {};
+var data = new vis.DataSet()
+var layers = {}
 data.on('*', function(event, properties, sender) {
 	properties.items.forEach(function(id) {
 		switch (event) {
 		case 'add':
-			var d = data.get(id);
+			var d = data.get(id)
 
-			layers[id] = L.geoJson(d['geojson']).addTo(map);
-			channel[id] = true;
-			break;
+			layers[id] = L.geoJson(d['geojson']).addTo(map)
+			channel[id] = true
+			break
 		case 'update':
-			var d = data.get(id);
-			var o = properties.data[id];
+			var d = data.get(id)
+			var o = properties.data[id]
 
-			layers[id].clearLayers();
-			layers[id].addData(d['geojson']);
-			break;
+			layers[id].clearLayers()
+			layers[id].addData(d['geojson'])
+			break
 		case 'remove':
-			map.removeLayer(layers[id]);
-			delete layers[id];
-			delete channel[id];
-			break;
+			map.removeLayer(layers[id])
+			delete layers[id]
+			delete channel[id]
+			break
 		}
-	});
-});
+	})
+})
 
-var now = new Date();
+var now = new Date()
 var timeline = new vis.Timeline($('#timeline').get(0), data, {
 	orientation: 'top',
 	type: 'point',
@@ -60,60 +60,60 @@ var timeline = new vis.Timeline($('#timeline').get(0), data, {
 		updateGroup: true,
 		remove: true,
 	},
-});
+})
 
-var xhr = {};
+var xhr = {}
 $('#channels #refresh').click(function(event) {
 	function cleanup() {
-		$('#channellist #refresh i').toggleClass('fa-spin');
-		delete xhr['refresh'];
+		$('#channellist #refresh i').toggleClass('fa-spin')
+		delete xhr['refresh']
 	}
 
 	if (xhr['refresh'] !== undefined) {
-		xhr['refresh'].abort();
-		cleanup();
-		return;
+		xhr['refresh'].abort()
+		cleanup()
+		return
 	}
 
-	$('#channellist #refresh i').toggleClass('fa-spin');
+	$('#channellist #refresh i').toggleClass('fa-spin')
 
 	xhr['refresh'] = $.ajax({
 		dataType: 'jsonp',
 		jsonp: 'callback',
 		url: '/channel?callback=?',
 		success: function(data) {
-			cleanup();
+			cleanup()
 
-			var p = {};
-			$('#channellist tr[id]').map(function() { p[this.id] = 1; });
+			var p = {}
+			$('#channellist tr[id]').map(function() { p[this.id] = 1 })
 
 			Object.keys(data.channels).filter(function(i) { return p[i] === undefined }).forEach(function(id) {
 				if (!data.channels[id].registered && !$('#channels #unregistered').hasClass('active'))
-					return;
+					return
 
-				var type = (data.channels[id].registered) ? 'history' : 'plus';
+				var type = (data.channels[id].registered) ? 'history' : 'plus'
 
-				$('#channellist > tbody').append('<tr id="'+id+'" class="fa-lg"><th style="width: 100%;">'+id+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="'+type+'"><a href="#"><i class="fa fa-'+type+'"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>');
-				p[id] = 1;
-			});
+				$('#channellist > tbody').append('<tr id="'+id+'" class="fa-lg"><th style="width: 100%;">'+id+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="'+type+'"><a href="#"><i class="fa fa-'+type+'"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>')
+				p[id] = 1
+			})
 
 			Object.keys(p).filter(function(id) { return data.channels[id] === undefined }).forEach(function(id) {
-				$('#devicelist #'+id).remove();
-				data.remove(id);
-			});
+				$('#devicelist #'+id).remove()
+				data.remove(id)
+			})
 		},
 		error: function(error) {
-			cleanup();
+			cleanup()
 		}
-	});
-});
-$('#channels #refresh').click();
+	})
+})
+$('#channels #refresh').click()
 
 function addRealtime(channel, geojson) {
-	var o = data.get(channel);
+	var o = data.get(channel)
 
 	if (o !== null && o.geojson.properties.time > geojson.properties.time)
-		return;
+		return
 
 	data.update({
 		id: channel,
@@ -121,108 +121,108 @@ function addRealtime(channel, geojson) {
 		content: channel,
 		start: new Date(geojson.properties.time * 1000),
 		geojson: geojson
-	});
+	})
 }
 
-var tag = 0;
-var channel = { };
-var connection = new WebSocket('ws://' + location.host);
+var tag = 0
+var channel = { }
+var connection = new WebSocket('ws://' + location.host)
 connection.onopen = function(){
 	function log(m, force){
 		if (force || debug)
-			console.log('GISrec:ws: '+m);
+			console.log('GISrec:ws: '+m)
 	}
 
 	function send(m){
-		m['tag'] = tag++;
-		connection.send(JSON.stringify(m));
-		return m['tag'];
+		m['tag'] = tag++
+		connection.send(JSON.stringify(m))
+		return m['tag']
 	}
 
-	log('connected');
+	log('connected')
 
 	connection.onclose = function(e){
-		log('disconnected: '+e);
-	};
+		log('disconnected: '+e)
+	}
 	connection.onmessage = function(e){
-		log("message: "+e.data);
+		log("message: "+e.data)
 
-		var message = JSON.parse(e.data);
+		var message = JSON.parse(e.data)
 
 		switch (message.type){
 		case 'error':
-			log("error: "+e.data, true);
-			break;
+			log("error: "+e.data, true)
+			break
 		case 'realtime':
 			if (channel[message.channel] === undefined) {	
-				log("realtime on non-joined channel '"+message.channel+"', leaving");
-				send({ type: 'error', text: "unsolicited realtime message for channel '"+message.channel+"', leaving" });
-				send({ type: 'prune', channel: [ message.channel ] });
-				break;
+				log("realtime on non-joined channel '"+message.channel+"', leaving")
+				send({ type: 'error', text: "unsolicited realtime message for channel '"+message.channel+"', leaving" })
+				send({ type: 'prune', channel: [ message.channel ] })
+				break
 			}
 
-			addRealtime(message.channel, message.geojson);
-			break;
+			addRealtime(message.channel, message.geojson)
+			break
 		case 'channel':
 			if (!$('#channels #unregistered').hasClass('active')) {
-				log("channel when not-requested, leaving");
-				send({ type: 'error', text: "unsolicited channel message, leaving" });
-				send({ type: 'prune', channel: [ null ] });
+				log("channel when not-requested, leaving")
+				send({ type: 'error', text: "unsolicited channel message, leaving" })
+				send({ type: 'prune', channel: [ null ] })
 
-				break;
+				break
 			}
 
 			if (!$('#channellist').find('#'+message.channel).length)
-				$('#channellist > tbody').append('<tr id="'+message.channel+'" class="fa-lg"><th style="width: 100%;">'+message.channel+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="plus"><a href="#"><i class="fa fa-plus"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>');
-			break;
+				$('#channellist > tbody').append('<tr id="'+message.channel+'" class="fa-lg"><th style="width: 100%;">'+message.channel+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="plus"><a href="#"><i class="fa fa-plus"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>')
+			break
 		default:
-			log('unknown message type: '+message.type, true);
+			log('unknown message type: '+message.type, true)
 		}
-	};
+	}
 
 	$('#channellist').click(function(event){
-		var i = $(event.target).closest('tr').attr('id');
-		var a = $(event.target).closest('td');
+		var i = $(event.target).closest('tr').attr('id')
+		var a = $(event.target).closest('td')
 
 		switch (a.attr('id')) {
 		case 'location-arrow':
-			a.toggleClass('inactive');
+			a.toggleClass('inactive')
 
-			var type;
+			var type
 			if (a.hasClass('inactive')) {
-				type = 'prune';
+				type = 'prune'
 
 				if (xhr['get channel '+i] !== undefined)
-					xhr['get channel '+i].abort();
-				data.remove(i);
+					xhr['get channel '+i].abort()
+				data.remove(i)
 			} else {
-				type = 'join';
+				type = 'join'
 
 				xhr['get channel '+i] = $.ajax({
 					dataType: 'jsonp',
 					jsonp: 'callback',
 					url: '/channel/'+i+'?callback=?',
 					success: function(geojson) {
-						delete xhr['get channel '+i];
-						addRealtime(i, geojson);
+						delete xhr['get channel '+i]
+						addRealtime(i, geojson)
 					},
 					error: function(error) {
-						delete xhr['get channel '+i];
+						delete xhr['get channel '+i]
 					}
-				});
+				})
 			}
 
 			send({
 				type: type,
 				channel: [ i ],
-			});
-			break;
+			})
+			break
 		case 'history':
-			a.toggleClass('inactive');
-			break;
+			a.toggleClass('inactive')
+			break
 		case 'plus':
 			if (xhr['put channel '+i] !== undefined)
-				xhr['put channel '+i].abort();
+				xhr['put channel '+i].abort()
 
 			xhr['put channel '+i] = $.ajax({
 				dataType: 'jsonp',
@@ -230,19 +230,19 @@ connection.onopen = function(){
 				type: 'PUT',
 				url: '/channel/'+i+'?callback=?',
 				success: function(data) {
-					delete xhr['put channel '+i];
-					$('#channellist #'+i).remove();
-					$('#channellist > tbody').append('<tr id="'+i+'" class="fa-lg"><th style="width: 100%;">'+i+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="history"><a href="#"><i class="fa fa-history"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>');
+					delete xhr['put channel '+i]
+					$('#channellist #'+i).remove()
+					$('#channellist > tbody').append('<tr id="'+i+'" class="fa-lg"><th style="width: 100%;">'+i+'</th><td class="gisrec inactive" id="location-arrow"><a href="#"><i class="fa fa-location-arrow"></i></a></td><td class="gisrec inactive" id="history"><a href="#"><i class="fa fa-history"></i></a></td><td class="gisrec inactive" id="trash"><a href="#"><i class="fa fa-trash"></i></a></td></tr>')
 				},
 				error: function(error) {
-					delete xhr['delete channel '+i];
+					delete xhr['delete channel '+i]
 				}
-			});
+			})
 
-			break;
+			break
 		case 'trash':
 			if (xhr['delete channel '+i] !== undefined)
-				xhr['delete channel '+i].abort();
+				xhr['delete channel '+i].abort()
 
 			xhr['delete channel '+i] = $.ajax({
 				dataType: 'jsonp',
@@ -250,24 +250,24 @@ connection.onopen = function(){
 				type: 'DELETE',
 				url: '/channel/'+i+'?callback=?',
 				success: function(data) {
-					delete xhr['delete channel '+i];
-					$('#channellist #'+i).remove();
-					data.remove(i);
+					delete xhr['delete channel '+i]
+					$('#channellist #'+i).remove()
+					data.remove(i)
 				},
 				error: function(error) {
-					delete xhr['delete channel '+i];
+					delete xhr['delete channel '+i]
 				}
-			});
-			break;
+			})
+			break
 		}
-	});
+	})
 
 	$('#channels #unregistered').click(function(event) {
-		$(this).button('toggle');
+		$(this).button('toggle')
 
 		send({
 			type: $(this).hasClass('active') ? 'join' : 'prune',
 			channel: [ null ],
-		});
-	});
-};
+		})
+	})
+}
--- server.js
+++ server.js
@@ -8,109 +8,109 @@ const WebSocketServer = require('ws').Server
 	, es = require('event-stream')
 	, fs = require('fs')
 	, rimraf = require('rimraf')
-	, url = require ('url');
+	, url = require ('url')
 
-const KNOTS_TO_METRES_PER_SECOND = 0.51444;
+const KNOTS_TO_METRES_PER_SECOND = 0.51444
 
-var client = {};
-var channel = {};
+var client = {}
+var channel = {}
 
 try {	// TODO check err
 	fs.statSync('data')
 } catch(e) {
 	try {
-		fs.mkdirSync('data');
+		fs.mkdirSync('data')
 	} catch (e) { }
 }
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 app.get('/channel', function(req, res) {
 	if (req.query.callback === undefined)
-		return res.status(400).jsonp({ text: "missing 'callback'" });
+		return res.status(400).jsonp({ text: "missing 'callback'" })
 
 	fs.readdir('data', function(err, files) {	// TODO check err and 'valid' chanel names
 		var channels = { }
 		files.map(function (c) {
 			if (fs.statSync('data/'+c).isFile() && /\.json$/.test(c))
-				channels[c.replace(/\.json$/, '')] = { registered: false };
+				channels[c.replace(/\.json$/, '')] = { registered: false }
 			else if (fs.statSync('data/'+c).isDirectory())
-				channels[c] = { registered: true };
-		});
-		res.jsonp({ channels: channels });
-	});
-});
+				channels[c] = { registered: true }
+		})
+		res.jsonp({ channels: channels })
+	})
+})
 app.all('/channel/*', function(req, res) {
 	if (req.query.callback === undefined)
-		return res.status(400).jsonp({ text: "missing 'callback'" });
+		return res.status(400).jsonp({ text: "missing 'callback'" })
 
-	var chan = url.parse(req.url).pathname.replace(/^\/channel\//, '');
+	var chan = url.parse(req.url).pathname.replace(/^\/channel\//, '')
 
 	if (!/^[0-9a-zA-Z]+$/.test(chan))
-		return res.status(400).jsonp({ text: "bad channel" });
+		return res.status(400).jsonp({ text: "bad channel" })
 
-	var channels = fs.readdirSync('data');
+	var channels = fs.readdirSync('data')
 	var s = channels.map(function(c) {
 		if (c === chan && fs.statSync('data/'+c).isDirectory()) {
-			var files = fs.readdirSync('data/'+c);
+			var files = fs.readdirSync('data/'+c)
 			if (files.length)
-				return c+'/'+files.sort()[0];
+				return c+'/'+files.sort()[0]
 		} else if (c === chan+'.json' && fs.statSync('data/'+c).isFile())
 			return c
-	}).filter(function(v) { return v !== undefined });
+	}).filter(function(v) { return v !== undefined })
 
 	if (s.length === 0)
-		return res.sendStatus(404);
+		return res.sendStatus(404)
 
 	switch (req.method) {
 	case 'GET':
 		if (s.length === 1) {
 			fs.readFile('data/'+s[0], function(err, data) {	// TODO check err
-				res.jsonp(JSON.parse(data));
-			});
+				res.jsonp(JSON.parse(data))
+			})
 		} else
-			res.status(409).jsonp({ text: 'registered and non-registered versions exist' });
-		break;
+			res.status(409).jsonp({ text: 'registered and non-registered versions exist' })
+		break
 	case 'PUT':
 		if (s.length === 1) {
 			if (fs.statSync('data/'+s[0]).isDirectory()) {
-				res.sendStatus(428);
-				break;
+				res.sendStatus(428)
+				break
 			}
 
 			fs.readFile('data/'+s[0], function(err, data) {	// TODO check err
-				var ts = new Date(JSON.parse(data).properties.time * 1000);
+				var ts = new Date(JSON.parse(data).properties.time * 1000)
 				fs.mkdir('data/'+chan, function(err) {	// TODO check err
 					fs.rename('data/'+s[0], 'data/'+chan+'/'+ts.toISOString()+'.json', function(err) { // TODO check err
-						res.sendStatus(204);
-					});
-				});
-			});
+						res.sendStatus(204)
+					})
+				})
+			})
 		} else
-			res.status(409).jsonp({ text: 'registered and non-registered versions exist' });
-		break;
+			res.status(409).jsonp({ text: 'registered and non-registered versions exist' })
+		break
 	case 'DELETE':
 		if (s.length === 1) {
 			rimraf('data/'+chan, function(err) {
-				res.sendStatus(204);
-			});
+				res.sendStatus(204)
+			})
 		} else
-			res.status(409).jsonp({ text: 'registered and non-registered versions exist' });
-		break;
+			res.status(409).jsonp({ text: 'registered and non-registered versions exist' })
+		break
 	default:
-		res.sendStatus(405);
+		res.sendStatus(405)
 	}
-});
+})
 
-var iid = 0;
+var iid = 0
 wss.on('connection', function(ws) {
-	var id = iid++;
-	client[id] = ws;
+	var id = iid++
+	client[id] = ws
 
 	var	remoteAddress = ws._socket.remoteAddress,
-		remotePort = ws._socket.remotePort;
+		remotePort = ws._socket.remotePort
 
 	function log(m) {
-		console.log('ws: ['+remoteAddress+']:'+remotePort+': '+m);
+		console.log('ws: ['+remoteAddress+']:'+remotePort+': '+m)
 	}
 
 	log("connected")
@@ -119,23 +119,23 @@ wss.on('connection', function(ws) {
 		log("disconnected")
 
 		Object.keys(channel).forEach(function(chan) {
-			channel[chan].splice(channel[chan].indexOf(id), 1);
+			channel[chan].splice(channel[chan].indexOf(id), 1)
 
 			if (channel[chan].length === 0)
-				delete channel[chan];
-		});
+				delete channel[chan]
+		})
 
-		delete client[id];
-	});
+		delete client[id]
+	})
 
 	ws.on('message', function(msg) {
-		log('message: '+msg);
+		log('message: '+msg)
 
-		var message = JSON.parse(msg);
+		var message = JSON.parse(msg)
 
 		if (message.type === 'error') {
-			log('error: '+message.text);
-			return;
+			log('error: '+message.text)
+			return
 		}
 
 		if (message.tag === undefined) {
@@ -143,64 +143,64 @@ wss.on('connection', function(ws) {
 				tag:	null,
 				type:	'error',
 				text:	'missing tag',
-			}));
-			return;
+			}))
+			return
 		}
 
 		message.channel.forEach(function(chan) {
-			var c = (chan !== null) ? chan : '';
+			var c = (chan !== null) ? chan : ''
 
 			switch (message.type) {
 			case 'join':
 				if (channel[c] === undefined)
-					channel[c] = [ id ];
+					channel[c] = [ id ]
 				else
-					channel[c].push(id);
+					channel[c].push(id)
 
-				break;
+				break
 			case 'prune':
 				if (channel[c] === undefined)
-					break;
+					break
 
-				channel[c].splice(channel[c].indexOf(id), 1);
+				channel[c].splice(channel[c].indexOf(id), 1)
 
 				if (channel[c].length === 0)
-					delete channel[c];
+					delete channel[c]
 
-				break;
+				break
 			default:
 				ws.send(JSON.stringify({
 					tag:	tag,
 					type:	'error',
 					text:	"unknown command '"+message.type+"'",
-				}));
-				break;
+				}))
+				break
 			}
-		});
-	});
-});
+		})
+	})
+})
 
-server.listen(process.env.PORT_HTTP || 27270, '::');
+server.listen(process.env.PORT_HTTP || 27270, '::')
 
 // http://aprs.gids.nl/nmea/#rmc
-const reGPRMC = /^\$GPRMC,([0-9]{6}(?:\.[0-9]+)?)?,([AV])?,([0-9]+(?:\.[0-9]+)?)?,([NS])?,([0-9]+(?:\.[0-9]+)?)?,([EW])?,([0-9]+(?:\.[0-9]+)?)?,([0-9]+(?:\.[0-9]+)?)?,([0-9]{6})?,([0-9]+(?:\.[0-9]+)?)?,([EW])?\*([0-9A-F]+)$/;
+const reGPRMC = /^\$GPRMC,([0-9]{6}(?:\.[0-9]+)?)?,([AV])?,([0-9]+(?:\.[0-9]+)?)?,([NS])?,([0-9]+(?:\.[0-9]+)?)?,([EW])?,([0-9]+(?:\.[0-9]+)?)?,([0-9]+(?:\.[0-9]+)?)?,([0-9]{6})?,([0-9]+(?:\.[0-9]+)?)?,([EW])?\*([0-9A-F]+)$/
 
 // http://www.yourgps.de/marketplace/products/documents/xexun/User-Manual-XT-009.pdf
-const reXEXUN = /^([0-9]+),(\+?[0-9]+),(GPRMC,.*,,),[A-Z](\*[0-9A-F]+),([FL]),([^,]*), ?imei:([0-9]*),([0-9]*),(-?[0-9]+(?:\.[0-9]+)?),([FL]):([0-9]+(?:\.[0-9]+)?)V,([01]),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9A-F]+),([0-9A-F]+)$/;
+const reXEXUN = /^([0-9]+),(\+?[0-9]+),(GPRMC,.*,,),[A-Z](\*[0-9A-F]+),([FL]),([^,]*), ?imei:([0-9]*),([0-9]*),(-?[0-9]+(?:\.[0-9]+)?),([FL]):([0-9]+(?:\.[0-9]+)?)V,([01]),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9A-F]+),([0-9A-F]+)$/
 
 var gis = net.createServer(function(sock) {
 	var	remoteAddress = sock.remoteAddress,
-		remotePort = sock.remotePort;
+		remotePort = sock.remotePort
 
 	function log(m) {
-		console.log('gis: ['+remoteAddress+']:'+remotePort+': '+m);
+		console.log('gis: ['+remoteAddress+']:'+remotePort+': '+m)
 	}
 
-	log('connected');
+	log('connected')
 
 	sock.on('close', function() {
-		log('disconnected');
-	});
+		log('disconnected')
+	})
 	sock.pipe(es.split()).pipe(es.map(function (data) {
 		var meta = {
 			'id':		null,
@@ -208,31 +208,31 @@ var gis = net.createServer(function(sock) {
 			'source':	{ address: remoteAddress, port: remotePort },
 			'recv-time':	(new Date())/1000.0,
 			'protocol':	[],
-		};
+		}
 
 		switch (true) {
 		case data === '':
-			break;
+			break
 		case reXEXUN.test(data):
-			log('received Xexun payload, converting to GPRMC');
+			log('received Xexun payload, converting to GPRMC')
 
-			meta.xexun		= {};
-			meta.protocol.push('xexun');
+			meta.xexun		= {}
+			meta.protocol.push('xexun')
 
-			var match = reXEXUN.exec(data);
-			meta.xexun.serial	= match[1];	// gps date + gps time
-			meta.xexun['admin-tel']	= match[2];
-			data			= '$'+match[3]+match[4];
-			meta.xexun['gps-fix']	= ( match[5] === 'F' ) ? 1 : 0;
-			meta.xexun.message	= match[6];
-			meta.xexun.imei		= match[7];
-			meta.xexun.satellites	= parseInt(match[8]);
-			meta.xexun.altitude	= parseFloat(match[9]);
+			var match = reXEXUN.exec(data)
+			meta.xexun.serial	= match[1]	// gps date + gps time
+			meta.xexun['admin-tel']	= match[2]
+			data			= '$'+match[3]+match[4]
+			meta.xexun['gps-fix']	= ( match[5] === 'F' ) ? 1 : 0
+			meta.xexun.message	= match[6]
+			meta.xexun.imei		= match[7]
+			meta.xexun.satellites	= parseInt(match[8])
+			meta.xexun.altitude	= parseFloat(match[9])
 			meta.xexun.battery	= {
 				charged: ( match[10] === 'F' ) ? 1 : 0,
 				voltage: parseFloat(match[11]),
 				charging: parseInt(match[12]),
-			};
+			}
 			meta.xexun.length	= parseInt(match[13]),
 			meta.xexun.crc16	= parseInt(match[14]),
 			meta.xexun.gsm	= {
@@ -240,31 +240,31 @@ var gis = net.createServer(function(sock) {
 				mnc: parseInt(match[16]),
 				lac: match[17],
 				cellid: match[18],
-			};
+			}
 
-			meta.id = meta.xexun.imei;
+			meta.id = meta.xexun.imei
 		case reGPRMC.test(data):
-			log('processing GPRMC');
-			meta.protocol.push('gprmc');
-			processGPRMC(data, meta);
-			break;
+			log('processing GPRMC')
+			meta.protocol.push('gprmc')
+			processGPRMC(data, meta)
+			break
 		default:
-			log("received unparsable data: '"+data+"', closing connection");
-			sock.end();
+			log("received unparsable data: '"+data+"', closing connection")
+			sock.end()
 		}
-	}));
+	}))
 
 	function processGPRMC(data, properties) {
 		if (properties.id === null) {
-			log("'id' is not set, unable to save data");
-			return;
+			log("'id' is not set, unable to save data")
+			return
 		}
 		if (!/^[0-9a-zA-Z]+$/.test(properties.id)) {
-			log("'id' is not valid, unable to save data");
-			return;
+			log("'id' is not valid, unable to save data")
+			return
 		}
 
-		var match = reGPRMC.exec(data);
+		var match = reGPRMC.exec(data)
 		var	time = match[1],
 			validity = ( match[2] === 'A' ) ? 1 : 0,
 			latitude = parseFloat(match[3]),	hemisphere = match[4],
@@ -273,83 +273,83 @@ var gis = net.createServer(function(sock) {
 			cmg = parseFloat(match[8]),
 			date = match[9],
 			magvar = parseFloat(match[10]),		maghandedness = match[11],
-			checksum = parseInt(match[12]);
+			checksum = parseInt(match[12])
 
-		var	dateParts = /([0-9]{2})([0-9]{2})([0-9]{2})/.exec(date);
-		var	d = dateParts[1], m = dateParts[2] - 1, y = '20'+dateParts[3];
-		var	timeParts = /([0-9]{2})([0-9]{2})([0-9]{2})(?:\.([0-9]{3}))/.exec(time);
-		var	H = timeParts[1], M = timeParts[2], S = timeParts[3], ms = timeParts[4] || 0;
+		var	dateParts = /([0-9]{2})([0-9]{2})([0-9]{2})/.exec(date)
+		var	d = dateParts[1], m = dateParts[2] - 1, y = '20'+dateParts[3]
+		var	timeParts = /([0-9]{2})([0-9]{2})([0-9]{2})(?:\.([0-9]{3}))/.exec(time)
+		var	H = timeParts[1], M = timeParts[2], S = timeParts[3], ms = timeParts[4] || 0
 
-		var	ts = new Date(y, m, d, H, M, S, ms);
+		var	ts = new Date(y, m, d, H, M, S, ms)
 
 		var point = {
 			latitude:	GPRMC2Degrees(latitude, hemisphere),
 			longitude:	GPRMC2Degrees(longitude, handedness),
-		};
+		}
 
-		properties.time				= parseFloat((ts/1000.0).toFixed(3));
+		properties.time				= parseFloat((ts/1000.0).toFixed(3))
 
-		properties.gprmc			= {};
-		properties.gprmc.raw			= data;
-		properties.gprmc.speed			= speed;
-		properties.gprmc['course-made-good']	= cmg;
-		properties.gprmc['magnetic-variance']	= GPRMC2Degrees(magvar, maghandedness);
+		properties.gprmc			= {}
+		properties.gprmc.raw			= data
+		properties.gprmc.speed			= speed
+		properties.gprmc['course-made-good']	= cmg
+		properties.gprmc['magnetic-variance']	= GPRMC2Degrees(magvar, maghandedness)
 		properties.gprmc.checksum		= checksum
 
-		var g = toGeoJSON(point, properties);
+		var g = toGeoJSON(point, properties)
 
 		fs.stat('data/'+properties.id, function(err, stat) {
 			if (err !== null && channel[''] !== undefined)
 				channel[''].forEach(function(i) {
-					log('channel to client '+i);
+					log('channel to client '+i)
 
 					client[i].send(JSON.stringify({
 						tag:		null,
 						type:		'channel',
 						channel:	properties.id,
-					}));
-				});
+					}))
+				})
 
 			function cb(err) {
 				if (err)
-					log('unable to save data: '+err);
+					log('unable to save data: '+err)
 
 				if (channel[properties.id] === undefined)
-					return;
+					return
 
 				channel[properties.id].forEach(function(i) {
-					log('realtime to client '+i);
+					log('realtime to client '+i)
 
 					client[i].send(JSON.stringify({
 						tag:		null,
 						type:		'realtime',
 						channel:	properties.id,
 						geojson:	g,
-					}));
-				});
-			};
+					}))
+				})
+			}
 
-			var name = properties.id;
+			var name = properties.id
 			if (err === null)
-				name = name.concat('/'+ts.toISOString());
-			fs.writeFile('data/'+name+'.json', JSON.stringify(g), cb);	// TODO temp file
-		}.bind(g));
+				name = name.concat('/'+ts.toISOString())
+			fs.writeFile('data/'+name+'.json', JSON.stringify(g), cb)	// TODO temp file
+		}.bind(g))
 
-		return;
+		return
 	}
-});
+})
 
-gis.listen(process.env.PORT_GIS || 27271, '::');
+gis.listen(process.env.PORT_GIS || 27271, '::')
 
 function GPRMC2Degrees(value, direction) {
 	// http://www.mapwindow.org/phorum/read.php?3,16271
-	var d = ((value/100) | 0) + (value - (((value/100) | 0) * 100)) / 60;
+	var d = ((value/100) | 0) + (value - (((value/100) | 0) * 100)) / 60
 
 	if (direction === 'S' || direction === 'W')
-		d *= -1;
+		d *= -1
 
 	// http://en.wikipedia.org/wiki/Decimal_degrees#Precision
-	return parseFloat(d.toFixed(5));
+	return parseFloat(d.toFixed(5))
 }
 
 function toGeoJSON(point, prop) {
@@ -360,7 +360,7 @@ function toGeoJSON(point, prop) {
 			coordinates:	[ point.longitude, point.latitude ],
 		},
 		properties:		prop,
-	};
+	}
 
-	return geojson;
+	return geojson
 }
